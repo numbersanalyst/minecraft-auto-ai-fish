811dfd4073dc023d472c8d96367d9ea05be4e62e
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-12-30 23:44:42 +0100

    Change pyautogui to pydirectinput-rgx and make fishing more real

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import numpy as np
 import cv2
 
 from time import sleep
-import pyautogui
+import pydirectinput
 import keyboard
 
 from colorama import Fore, Style, just_fix_windows_console, init
 
+
 def getData(capture_size: int) -> np.ndarray:
     """Takes a screenshot of the game and returns it as a numpy array."""
-    x, y = pyautogui.position()
+    x, y = pydirectinput.position()
     img = ImageGrab.grab(bbox=(x, y, x + capture_size, y + capture_size))
     data = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
     return data
@@ -23,10 +24,15 @@ def toggleScript() -> None:
     print(Style.DIM + 'Script loop is now', Fore.GREEN + 'active.' if script_active else Fore.RED + 'inactive.')
 
 
+def moveAndBack(length: int=380) -> None:
+    pydirectinput.moveRel(None, length, duration=0.2, relative=True)
+    sleep(0.2)
+    pydirectinput.moveRel(None, -length, duration=1, relative=True)
+
 
 def autoFish(capture_size: int, threshold: int, interval: float) -> None:
     """Automatically casts the rod and catches something."""
-    pyautogui.rightClick()
+    pydirectinput.rightClick()
     print('Casting the rod.')
     sleep(5)
     print('Waiting for something...')
@@ -36,14 +42,17 @@ def autoFish(capture_size: int, threshold: int, interval: float) -> None:
         data = getData(capture_size)
         sleep(interval)
         # PREVIEW DATA
-        # cv2.imshow('Preview', data)
-        # if cv2.waitKey(25) & 0xFF == ord('q'):
-        #     cv2.destroyAllWindows()
-        #     break
+        cv2.imshow('Preview', data)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
 
-    pyautogui.rightClick()
+
+    pydirectinput.rightClick()
     print('Catch!')
-    sleep(1)
+    sleep(0.15)
+    moveAndBack()
+
 
 
 def main():
@@ -62,7 +71,7 @@ def main():
 
     while True:
         while script_active:
-            autoFish(15, 10, 0.2)
+            autoFish(25, 15, 0.2)
 
         if keyboard.is_pressed('q'):
             break
@@ -72,3 +81,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
