a95330c9f2b86b8eac9e15a6b401a2e61733e77d
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-12-31 20:54:02 +0100

    Change the way it works, better reliability

--- main.py
+++ main.py
@@ -1,84 +1,102 @@
-from PIL import ImageGrab
+import cv2 as cv
 import numpy as np
-import cv2
-
-from time import sleep
-import pydirectinput
+from PIL import ImageGrab
+import pydirectinput as pdi
+from time import sleep, time
 import keyboard
-
+import os
 from colorama import Fore, Style, just_fix_windows_console, init
 
-
-def getData(capture_size: int) -> np.ndarray:
-    """Takes a screenshot of the game and returns it as a numpy array."""
-    x, y = pydirectinput.position()
-    img = ImageGrab.grab(bbox=(x, y, x + capture_size, y + capture_size))
-    data = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
-    return data
-
-
-def toggleScript() -> None:
-    """Toggles the script loop."""
-    global script_active
-    script_active = not script_active
-    print(Style.DIM + 'Script loop is now', Fore.GREEN + 'active.' if script_active else Fore.RED + 'inactive.')
+# Settings
+capture_size = 150
+threshold = 0.6
+interval = 0.25
+detection = False
+reaction = True
+template = cv.imread("bobber_full.png")
+detected_time = time()
 
 
-def moveAndBack(length: int=380) -> None:
-    pydirectinput.moveRel(None, length, duration=0.2, relative=True)
-    sleep(0.2)
-    pydirectinput.moveRel(None, -length, duration=1, relative=True)
-
-
-def autoFish(capture_size: int, threshold: int, interval: float) -> None:
-    """Automatically casts the rod and catches something."""
-    pydirectinput.rightClick()
-    print('Casting the rod.')
-    sleep(5)
-    print('Waiting for something...')
-
-    data = getData(capture_size)
-    while np.sum(data == 0) > threshold:
-        data = getData(capture_size)
-        sleep(interval)
-        # PREVIEW DATA
-        cv2.imshow('Preview', data)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
-
-
-    pydirectinput.rightClick()
-    print('Catch!')
-    sleep(0.15)
-    moveAndBack()
-
-
-
-def main():
+def setup_colorama():
     just_fix_windows_console()
     init(autoreset=True)
 
-    global script_active
-    script_active = False
 
-    print(Style.BRIGHT +'Minecraft Auto Fishing Script for Mausaa')
-    print(Style.DIM + 'Press ctrl to toggle the script.')
-    print(Style.DIM + 'Press q to quit program.')
+def toggle_script():
+    global detection
+    detection = not detection
+    # Mesage to user
+    print(
+        Style.DIM + "Script loop is now",
+        Fore.GREEN + "active." if detection else Fore.RED + "inactive.",
+    )
+
+
+def takeFish():
+    print("Taking fish!")  # Mesage to user
+    pdi.rightClick()
+    pdi.moveRel(None, 380, 0.25, relative=True, disable_mouse_acceleration=True)
+    sleep(0.2)
+    pdi.moveRel(None, -380, 0.25, relative=True, disable_mouse_acceleration=True)
+    sleep(1)
+    pdi.rightClick()
+    sleep(3)
+
+
+if __name__ == "__main__":
+    setup_colorama()
+
+    keyboard.add_hotkey("p", toggle_script)  # Press p to toggle the script
+    keyboard.add_hotkey("o", lambda: os._exit(0))  # Press o to quit
+
+    print(Style.BRIGHT + "Minecraft Auto Fishing AI Script for Mausaa")
+    print(Style.DIM + "Press p to toggle the script.")
+    print(Style.DIM + "Press o to quit program.")
     print()
 
-    keyboard.add_hotkey('ctrl', toggleScript)
-
     while True:
-        while script_active:
-            autoFish(25, 15, 0.2)
+        if detection:
+            x, y = pdi.position()
+            img = cv.cvtColor(
+                np.array(
+                    ImageGrab.grab(
+                        bbox=(
+                            x - capture_size,
+                            y - capture_size,
+                            x + capture_size,
+                            y + capture_size,
+                        )
+                    )
+                ),
+                cv.COLOR_RGB2BGR,
+            )
 
-        if keyboard.is_pressed('q'):
-            break
+            res = cv.matchTemplate(img, template, cv.TM_CCOEFF_NORMED)
+            loc = np.where(res >= threshold)
 
-    keyboard.remove_hotkey('ctrl')
+            # Detect
+            if reaction:
+                if np.sum(loc) != 0:
+                    detected_time = time()
+                else:
+                    if time() - detected_time > 0.35:
+                        takeFish()
 
+            # Draw rectangles
+            for pt in zip(*loc[::-1]):
+                cv.rectangle(
+                    img,
+                    pt,
+                    (pt[0] + template.shape[1], pt[1] + template.shape[0]),
+                    (0, 255, 0),
+                    1,
+                )
 
-if __name__ == '__main__':
-    main()
-    
+            # Save image
+            # cv.imwrite("res.png", img)
+
+            # Show preview
+            cv.imshow("Preview", img)
+            cv.waitKey(1)
+
+            sleep(interval)
