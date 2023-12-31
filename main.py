22ff8e36acbcc7a25257a3b4895559002e9a62e1
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-12-31 23:50:01 +0100

    Code refactorization

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ import os
 from colorama import Fore, Style, just_fix_windows_console, init
 
 # Settings
-capture_size = 150
-threshold = 0.6
-interval = 0.25
+CAPTURE_SIZE = 150
+THRESHOLD = 0.6
+INTERVAL = 0.25
+REACTION_TIME_THRESHOLD = 0.35
+
 detection = False
-reaction = True
-template = cv.imread("bobber_full.png")
+reaction = False
 detected_time = time()
+template = cv.imread("bobber_full.png")
 
 
 def setup_colorama():
@@ -25,15 +27,15 @@ def setup_colorama():
 def toggle_script():
     global detection
     detection = not detection
-    # Mesage to user
+    # Message to user
     print(
         Style.DIM + "Script loop is now",
         Fore.GREEN + "active." if detection else Fore.RED + "inactive.",
     )
 
 
-def takeFish():
-    print("Taking fish!")  # Mesage to user
+def take_fish():
+    print("Taking fish!")  # Message to user
     pdi.rightClick()
     pdi.moveRel(None, 380, 0.25, relative=True, disable_mouse_acceleration=True)
     sleep(0.2)
@@ -41,6 +43,63 @@ def takeFish():
     sleep(1)
     pdi.rightClick()
     sleep(3)
+
+
+def process_image():
+    x, y = pdi.position()
+    img = cv.cvtColor(
+        np.array(
+            ImageGrab.grab(
+                bbox=(
+                    x - CAPTURE_SIZE,
+                    y - CAPTURE_SIZE,
+                    x + CAPTURE_SIZE,
+                    y + CAPTURE_SIZE,
+                )
+            )
+        ),
+        cv.COLOR_RGB2BGR,
+    )
+    return img
+
+
+def detect_fish(img):
+    res = cv.matchTemplate(img, template, cv.TM_CCOEFF_NORMED)
+    loc = np.where(res >= THRESHOLD)
+
+    # Reaction
+    if reaction:
+        global detected_time
+        if np.sum(loc) != 0:
+            detected_time = time()
+        elif time() - detected_time > REACTION_TIME_THRESHOLD:
+            take_fish()
+
+    # Draw rectangles
+    for pt in zip(*loc[::-1]):
+        cv.rectangle(
+            img,
+            pt,
+            (pt[0] + template.shape[1], pt[1] + template.shape[0]),
+            (0, 255, 0),
+            1,
+        )
+
+    return img
+
+
+def show_preview(img):
+    cv.imshow("Preview", img)
+    cv.waitKey(1)
+
+
+def main_loop():
+    while True:
+        if detection:
+            screen = process_image()
+            img_with_detection = detect_fish(screen)
+            show_preview(img_with_detection)
+            sleep(INTERVAL)
 
 
 if __name__ == "__main__":
@@ -54,49 +113,4 @@ if __name__ == "__main__":
     print(Style.DIM + "Press o to quit program.")
     print()
 
-    while True:
-        if detection:
-            x, y = pdi.position()
-            img = cv.cvtColor(
-                np.array(
-                    ImageGrab.grab(
-                        bbox=(
-                            x - capture_size,
-                            y - capture_size,
-                            x + capture_size,
-                            y + capture_size,
-                        )
-                    )
-                ),
-                cv.COLOR_RGB2BGR,
-            )
-
-            res = cv.matchTemplate(img, template, cv.TM_CCOEFF_NORMED)
-            loc = np.where(res >= threshold)
-
-            # Detect
-            if reaction:
-                if np.sum(loc) != 0:
-                    detected_time = time()
-                else:
-                    if time() - detected_time > 0.35:
-                        takeFish()
-
-            # Draw rectangles
-            for pt in zip(*loc[::-1]):
-                cv.rectangle(
-                    img,
-                    pt,
-                    (pt[0] + template.shape[1], pt[1] + template.shape[0]),
-                    (0, 255, 0),
-                    1,
-                )
-
-            # Save image
-            # cv.imwrite("res.png", img)
-
-            # Show preview
-            cv.imshow("Preview", img)
-            cv.waitKey(1)
-
-            sleep(interval)
+    main_loop()
