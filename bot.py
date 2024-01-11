80d9281b8f5f75d28450002ada80be42319c2e05
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2024-01-11 22:02:37 +0100

    Make bot.py more easier, fix problem with not running function

--- bot.py
+++ bot.py
@@ -1,38 +1,19 @@
 from multiprocessing import Process
 
+
 class Bot:
     def __init__(self):
         self.detection = False
         self.reaction = False
         self.verification = False
-    
+
     def toggle_detection(self):
         self.detection = not self.detection
         if self.detection:
-            self.detection_process = Process(target=start_detection)
-        elif self.detection.is_alive():
+            self.detection_process = Process(target=self.start_detection)
+            self.detection_process.start()
+        elif self.detection_process.is_alive():
             self.detection_process.terminate()
-
-    def toggle_reaction(self):
-        self.reaction = not self.reaction
-        if self.reaction:
-            self.reaction_process = Process(target=start_reaction)
-        elif self.reaction.is_alive():
-            self.reaction_process.terminate()
-
-    def toogle_verification(self):
-        self.verification = not self.verification
-        if self.verification:
-            self.verification_process = Process(target=start_verification)
-        elif self.verification.is_alive():
-            self.verification_process.terminate()
 
     def start_detection(self):
         pass
-
-    def start_reaction(self):
-        pass
-    
-    def start_verification(self):
-        pass
-    
