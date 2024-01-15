dbac5e356d43ec7687f55b0f708002acd7505e66
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2024-01-16 00:28:46 +0100

    Update bot.py, integrate detection, add comments

--- bot.py
+++ bot.py
@@ -1,23 +1,35 @@
 from multiprocessing import Process
+from detection import Detector
+from time import sleep
 
 
 class Bot:
     def __init__(self, settings):
+        """Initialize the bot general values."""
         self.detection = False
         self.reaction = False
         self.verification = False
 
+        self.detection_ctrl = Detector(settings, self)
+
         self.settings = settings
 
     def toggle_detection(self):
+        """Toggles the detection process."""
         self.detection = not self.detection
         if self.detection:
+            # Start the detection process
             self.detection_process = Process(target=self.start_detection)
             self.detection_process.start()
         elif self.detection_process.is_alive():
+            # Stop the detection process
+            self.detection_ctrl.destroy_window()
             self.detection_process.terminate()
 
     def start_detection(self):
-        print(self.reaction, "reaction")
-        print(self.verification, "verification")
-        print(self.settings.capture_size)
+        """Detection process."""
+        self.detection_ctrl.create_window()
+        while True:
+            self.detection_ctrl.detect_bobber()
+            wait_time = float(self.settings.data["detection_interval"])
+            sleep(wait_time)
