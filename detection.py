b32ba24ae654371889b50f854f8ecf7cbb27d5d4
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2024-01-16 00:23:02 +0100

    Update detection.py, refactor code, add new functions, integrate reaction.py

--- detection.py
+++ detection.py
@@ -1,8 +1,8 @@
 from PIL import ImageGrab
-from time import sleep, time
 import pydirectinput as pdi
 import numpy as np
 import cv2 as cv
+from time import time
 
 from reaction import Reaction
 
@@ -18,109 +18,140 @@ class Detect:
         self.detected_time = time()
 
         self.eq_hsv = np.array([0, 0, 139])
-        self.verification_hsv_main = np.array([180, 100, 66.7])
-        self.verification_hsv_second = np.array([180, 100, 16.5])
+        self.verification_hsv_main = np.array([110, 130, 150])
+        self.verification_hsv_second = np.array([180, 255, 255])
 
         self.bobber_template = cv.imread("assets/bobber.png")
         self.skull_template = cv.imread("skull_human.png")
-        self.skull_template = cv.cvtColor(self.skull_template, cv.COLOR_BGR2HSV)
-        self.skull_template = cv.inRange(self.skull_template, self.eq_hsv, self.eq_hsv)
+        self.skull_template = self.proccess_image_with_hsv(
+            self.skull_template, self.eq_hsv
+        )
         self.skull_template = cv.blur(self.skull_template, (2, 2))
-
-        self.bobber_w, self.bobber_h = (
-            self.bobber_template.shape[1],
-            self.bobber_template.shape[0],
-        )
-        self.skull_w, self.skull_h = (
-            self.skull_template.shape[1],
-            self.skull_template.shape[0],
+        self.verification_template = cv.imread("assets/verification.png")
+        self.verification_template = self.proccess_image_with_hsv(
+            self.verification_template,
+            self.verification_hsv_main,
+            self.verification_hsv_second,
         )
 
-    def get_screen(self):
+        self.verification_half_w = 150
+        self.verification_h = 100
+
+        self.eq_half_w = 170
+        self.eq_h = 120
+
+    def get_screen(self, x1=None, y1=None, x2=None, y2=None):
         """Captures the screen and returns it as an image."""
         x, y = pdi.position()
+        if x1:  # Use custom coordinates
+            bbox = (x - x1, y - y1, x + x2, y + y2)
+        else:  # Use coordinates from settings
+            bbox = (
+                x - self.settings.capture_size[0],
+                y - self.settings.capture_size[1],
+                x + self.settings.capture_size[2],
+                y + self.settings.capture_size[3],
+            )
         img = cv.cvtColor(
-            np.array(
-                ImageGrab.grab(
-                    bbox=(
-                        x - self.settings.capture_size[0],
-                        y - self.settings.capture_size[1],
-                        x + self.settings.capture_size[2],
-                        y + self.settings.capture_size[3],
-                    )
-                )
-            ),
+            np.array(ImageGrab.grab(bbox)),
             cv.COLOR_RGB2BGR,
         )
         return img
 
-    def detect(self, img):
-        """Detect a bobber and return the image with a rectangle around the detected bobber."""
-        res = cv.matchTemplate(img, self.bobber_template, cv.TM_CCOEFF_NORMED)
-        _, max_val, _, max_loc = cv.minMaxLoc(res)
+    def detect(
+        self, img, template, threshold=0.8, hsv=False, hsv_max=False, blur=False
+    ):
+        """Finds and draws a rectangle around the template in the image. Returns image and position of the target."""
+        org_img = img  # Original image for preview
 
-        # Consider a match only if it exceeds the threshold
-        if max_val >= float(self.settings.data["detection_threshold"]):
-            self.detected_time = time()
+        if isinstance(hsv, np.ndarray):
+            if isinstance(hsv_max, np.ndarray):
+                img = self.proccess_image_with_hsv(img, hsv, hsv_max)
+            else:
+                img = self.proccess_image_with_hsv(img, hsv)
+        if blur:
+            img = cv.blur(img, (2, 2))
 
+        detection_result = cv.matchTemplate(img, template, cv.TM_CCOEFF_NORMED)
+        _, max_val, _, max_loc = cv.minMaxLoc(detection_result)
+
+        if max_val >= threshold:
             top_left = max_loc
             bottom_right = (
-                top_left[0] + self.bobber_w,
-                top_left[1] + self.bobber_h,
+                top_left[0] + template.shape[1],
+                top_left[1] + template.shape[0],
             )
+            cv.rectangle(org_img, top_left, bottom_right, (250, 0, 0), 2)
 
-            # Draw rectangle around the detected fish
-            cv.rectangle(img, top_left, bottom_right, (255, 0, 0), 2)
+            return org_img, top_left
 
+        # If no match was found
+        return org_img, None
+
+    def proccess_image_with_hsv(self, img, hsv, hsv_max=None):
+        """Process image with hsv filter"""
+        img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        if isinstance(hsv_max, np.ndarray):
+            img = cv.inRange(img, hsv, hsv_max)
+        else:
+            img = cv.inRange(img, hsv, hsv)
+
+        return img
+
+    def detect_bobber(self):
+        """Detects a bobber and returns the image with a rectangle around it. Invokes reaction if necessary."""
+        img = self.get_screen()
+        res, top_left = self.detect(
+            img, self.bobber_template, float(self.settings.data["detection_threshold"])
+        )
+        self.update_window(res)
+        if top_left != None:
+            self.detected_time = time()
         elif (
             time() - self.detected_time > float(self.settings.data["reaction_time"])
             and self.bot.reaction
         ):
             self.reaction.take_fish()
+            if self.bot.verification:
+                self.detect_verification()
+            pdi.rightClick()
 
-        return img
-
-    def detect_skull(self, img):
-        """Finds and draws a rectangle around the skull in the image. Returns image and the coordinates of the target."""
-        hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-        mask = cv.inRange(hsv_img, self.eq_hsv, self.eq_hsv)
-        mask = cv.blur(mask, (2, 2))
-
-        detection_result = cv.matchTemplate(
-            mask, self.skull_template, cv.TM_CCOEFF_NORMED
+    def detect_verification(self):
+        """Detects a verification and returns the image with a rectangle around it. Invokes reaction if necessary."""
+        img = self.get_screen(
+            self.verification_half_w, self.verification_h, self.verification_half_w, 0
         )
-        _, max_val, _, max_loc = cv.minMaxLoc(detection_result)
+        res, top_left = self.detect(
+            img,
+            self.verification_template,
+            hsv=self.verification_hsv_main,
+            hsv_max=self.verification_hsv_second,
+        )
+        self.update_window(res)
+        if top_left != None:
+            self.reaction.open_inventory()
+            self.detect_skull()
 
-        threshold = 0.8
-        if max_val > threshold:
-            top_left = max_loc
-            bottom_right = (
-                top_left[0] + self.skull_w,
-                top_left[1] + self.skull_h,
-            )
-            cv.rectangle(img, top_left, bottom_right, (250, 0, 0), 2)
+    def detect_skull(self):
+        """Detects a skull and returns the image with a rectangle around it. Invokes reaction if necessary."""
+        img = self.get_screen(self.eq_half_w, 0, self.eq_half_w, self.eq_h)
+        res, top_left = self.detect(
+            img, self.skull_template, hsv=self.eq_hsv, blur=True
+        )
+        self.update_window(res)
+        if top_left != None:
+            x, y = self.reaction.calculate_skull_position(top_left, self.eq_half_w)
+            self.reaction.bypass_verification(x, y)
 
-            return img, top_left
+    def create_window(self):
+        """Creates the preview window."""
+        cv.namedWindow("Preview", cv.WINDOW_NORMAL)
 
-        return hsv_img, None
+    def update_window(self, img):
+        """Updates the window with the given image."""
+        cv.imshow("Preview", img)
+        cv.waitKey(1)
 
-    def show_img(self, img):
-        """Shows the image."""
-        cv.imshow("image", img)
-        cv.waitKey(5000)
-
-
-if __name__ == "__main__":
-    from config import Settings
-    from bot import Bot
-
-    settings = Settings()
-    bot = Bot(settings)
-
-    detection = Detect(settings, bot)
-
-    img = detection.get_screen()
-
-    res, top_left = detection.detect_skull(img)
-
-    detection.show_img(res)
+    def destroy_window(self):
+        """Destroys the window."""
+        cv.destroyAllWindows()
