34284e39d7518ebeb91c9d19ad5c8ed8fbf66231
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2024-01-16 16:43:50 +0100

    Update reaction.py, add comments

--- reaction.py
+++ reaction.py
@@ -4,11 +4,13 @@ from time import sleep
 
 class Reaction:
     def __init__(self, settings):
+        """Get values from settings."""
         self.verification_time = settings.data["reaction_speed"]
         self.reaction_time = settings.data["reaction_time"]
         self.reaction_strength = settings.data["reaction_strength"]
 
     def take_fish(self):
+        """Controls the mouse to take a fish."""
         pdi.rightClick()
         pdi.moveRel(
             None,
@@ -30,6 +32,7 @@ class Reaction:
         sleep(1)
 
     def calculate_skull_position(self, loc, eq_half_w):
+        """Calculate the position of the skull in the inventory."""
         x, y = loc
 
         mid = eq_half_w - 20
@@ -39,11 +42,13 @@ class Reaction:
         return x, y
 
     def open_inventory(self):
+        """Opens inventory in the game."""
         pdi.press("e")
         sleep(0.2)
         pdi.moveRel(None, -5, relative=True)
 
     def bypass_verification(self, x, y):
+        """Selects detected skull in inventory."""
         pdi.moveRel(
             x,
             y,
